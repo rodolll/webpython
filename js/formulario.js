31b492f676ff1533e569e3c4ea9c4b976087a090
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-10-26 12:10:36 -0300

    arreglando formulario con formspree

--- js/formulario.js
+++ js/formulario.js
@@ -1,10 +1,10 @@
-const form = document.getElementById("myForm");
-const id = "moqorkrz"
-form.setAttribute("action", `https://formspree.io/f/${id}`);
+const form = document.querySelector("#myForm");
+form.setAttribute("action", `https://formspree.io/f/moqorkrz`);
 
 const nombre = form.elements.namedItem('fullName')
 const correo = form.elements.namedItem('_replyto');
 const mensaje = form.elements.namedItem('message');
+const honeypot = form.elements.namedItem("_gotcha")
 
 nombre.addEventListener('invalid', () => {
   nombre.classList.add("invalid");
@@ -28,13 +28,10 @@ mensaje.addEventListener("input", function(event) {
 });
 
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", handleSubmit);
 
-  const { elements } = event.currentTarget
-  
-  const telefono = elements.namedItem("phone")
-  const honeypot = elements.namedItem("_gotcha")
+async function handleSubmit(event) {
+  event.preventDefault();
   // Validar el campo honeypot, para evitar bots de spam
   if (honeypot.value) {
     return false;
@@ -43,28 +40,29 @@ form.addEventListener("submit", (event) => {
   const nombreRegex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]+$/;
   if (!nombreRegex.test(nombre.value)) {
     nombre.classList.add("invalid")
-    alert("Nombre inválido")
+    alert("Nombre:\nSolo se aceptan letras y espacios")
     return false
   }
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(correo.value)) {
     correo.classList.add("invalid")
-    alert("Correo inválido\nEscriba un correo válido")
-    return false
-  }
-  if (mensaje.value.length < 15) {
-    mensaje.classList.add("invalid")
-    alert('El campo de mensaje es inválido.\nEl mensaje debe contener por lo menos 15 carácteres.');
+    alert("Email:\nEscriba un correo válido")
     return false
   }
 
-  // Si todos los campos del formulario son válidos
-  nombre.value = ""
-  correo.value = ""
-  mensaje.value = ""
-  telefono.value = ""  
-  alert("Formulario enviado correctamente, gracias por su mensaje")
-  event.currentTarget.requestSubmit()
-  return true
-});
+  const data = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: data,
+    headers: {
+      "Accept": "application/json"
+    }
+  }) 
+  if(response.ok) {
+    alert("Formulario envia correctamente\nGracias por su mensaje.")
+    this.reset()
+  } else {
+    alert("Mensaje no enviado, ocurrió un problema con el servidor")
+  }
+};
 
