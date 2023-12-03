9919be8c7fd08128105d888824d1c40c98800419
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-12-02 22:03:20 -0300

    feat: haciendo fetching al backend

--- js/detalle_producto.js
+++ js/detalle_producto.js
@@ -1,3 +1,69 @@
+const url = 'http://127.0.0.1:8000'
+
+const urlParams = new URLSearchParams(window.location.search)
+const id_producto = urlParams.get('producto') // obteniendo el id del producto
+
+
+const obtenerDatos = async () => {
+  try {
+    const response = await fetch(`${url}/mueble/${id_producto}/`);
+    if (response.ok) {
+      const data = await response.json();
+      mostrarDatosEnHTML(data);
+    } else {
+      mostrarErrorHTML();
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+}
+
+
+const mostrarErrorHTML = () => {
+  let html = `
+  <article class="error">
+      <h1>
+        No existe el producto que esta buscando :/
+      </h1>
+      <a href="../index.html"> Volver a inicio</a>
+    </article>
+  `
+ 
+  const detalleHtml = document.querySelector('main')
+  detalleHtml.insertAdjacentHTML('beforeend', html)
+}
+
+
+const mostrarDatosEnHTML = (data) => {
+  let html = `
+  <article class="card">
+      <div>
+        <img src=${data.imagen} alt=${data.nombre} class="img-producto">
+      </div>
+      <div class="info">
+        <div>
+          <h1 class="titulo">${data.nombre}</h1>
+          <h2 class="descripcion">${data.descripcion ? data.descripcion : ''}</h2>
+        </div>
+        <div>  
+          <p class="color">${data.color ? `Color: ${data.precio}` : ''}</p>
+          <p class="medida">${data.medidas ? `Medidas: ${data.medidas}` : ''}</p>
+          <strong class="precio">Precio: $${data.precio}</strong>
+        </div>
+      </div>
+    </article>
+      `
+ 
+  const detalleHtml = document.querySelector('main')
+  detalleHtml.insertAdjacentHTML('beforeend', html)
+}
+      
+obtenerDatos();  // Llamada a la función
+
+
+/* 
+Obtiene los datos del producto y los muestra por pantalla - sin conectar al backend
+
 const lista_productos = [
   {
     titulo: "Mesa Redonda",
@@ -49,9 +115,6 @@ const lista_productos = [
   },
 ]
 
-const urlParams = new URLSearchParams(window.location.search)
-const id_producto = parseInt(urlParams.get('producto'))
-
 let html
 
 if(isNaN(id_producto) || (0 >= id_producto || id_producto > lista_productos.length)) {
@@ -88,3 +151,4 @@ if(isNaN(id_producto) || (0 >= id_producto || id_producto > lista_productos.leng
 
 const detalleHtml = document.querySelector('main')
 detalleHtml.insertAdjacentHTML('beforeend', html)
+*/
---
+++ js/productos.js
@@ -0,0 +1,46 @@
+const url = 'http://127.0.0.1:8000'
+
+const obtenerDatos = async () => {
+  try {
+    const response = await fetch(`${url}/all_muebles/`);
+    if (response.ok) {
+      const data = await response.json();
+      mostrarDatosEnHTML(data);
+    } else {
+      console.error('Error al obtener los datos');
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+}
+
+
+const mostrarDatosEnHTML = (data) => {
+  let html = ''
+  if (data.length == 0) {
+    html = `
+      <article class="product-card">
+        <h1>No hay productos por mostrar :/</h1>
+      </article>
+    `
+  } else {
+
+    for(let i = 0; i < data.length; i++) {
+      html += `
+        <article class="product-card">
+          <h1>${data[i].nombre}</h1>
+          <img src=${data[i].imagen} alt=${data[i].nombre}>
+          <button>
+            <a href="../detalle_producto.html?producto=${data[i].id}"> Detalle ${data[i].nombre}</a>
+          </button>
+        </article>
+      `
+    }
+  }
+ 
+  const productosHtml = document.querySelector('.list-products')
+  productosHtml.insertAdjacentHTML('beforeend', html)
+}
+      
+// Llamada a la función
+obtenerDatos();
