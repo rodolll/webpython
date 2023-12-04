c902250ed6e2084a9eeb48eb068bea1760ce9683
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-12-04 17:04:25 -0300

    agregando icono de usuario a navbar

--- js/header.js
+++ js/header.js
@@ -12,3 +12,42 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+// Obteniendo token
+const access = localStorage.getItem('access')
+
+function decodificarToken(token) {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  const decoded = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(''))
+ 
+  return JSON.parse(decoded)
+
+}
+
+
+const user = document.querySelectorAll('.user')
+const not_user = document.querySelectorAll('.not_user')
+
+if (access) {
+
+  const token = decodificarToken(access)
+  for (let i = 0; i < not_user.length; i++) {
+    not_user[i].style.display = 'none'
+  }
+  const img = document.getElementById('img_user')
+  if (token.photo) {
+    img.src = `http://127.0.0.1:8000${token.photo}/`
+  } else {
+    img.src = '../images/user.png'
+  }
+
+} else {
+  console.log('Usuario no autenticado');
+  for(let i = 0; i < user.length; i++) {
+    user[i].style.display = 'none'
+  }
+}
+
---
+++ js/registro.js
@@ -0,0 +1,48 @@
+const url = 'http://127.0.0.1:8000'
+
+const error = document.getElementById('error')
+
+document.getElementById('registerForm').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const username = document.getElementById('username');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const confirm_password = document.getElementById('confirm_password');
+
+  username.addEventListener('input', () => {
+    error.innerHTML = ''
+  })
+  email.addEventListener('input', () => {
+    error.innerHTML = ''
+  })
+  password.addEventListener('input', () => {
+    error.innerHTML = ''
+  })
+  confirm_password.addEventListener('input', () => {
+    error.innerHTML = ''
+  })
+
+ 
+  const response = await fetch(`${url}/register/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username.value, email: email.value,
+                             password: password.value, confirm_password: confirm_password.value })
+    });
+    const data = await response.json()
+    console.log("Data", data);
+
+    if(!response.ok) {
+      console.log("Error",`${data.error?.non_field_errors || data.error?.username || data.error?.email}`);
+      error.innerHTML = `${data.error?.non_field_errors || data.error?.username || data.error?.email }`
+      return
+    }
+
+    alert('Usuario creado correctamente')
+    localStorage.setItem('access', data.access)
+    localStorage.setItem('refresh', data.refresh)
+    window.location.href = '../index.html'
+});
+
